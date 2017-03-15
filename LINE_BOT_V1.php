9d113334505b0544ec1ffa150b875132cd8f9e50
571998011 <thanapoom.son@crru.ac.th>
2017-03-15 11:45:19 +0700

    Create LINE_BOT_V1.php

---
+++ LINE_BOT_V1.php
@@ -0,0 +1,48 @@
+<?php
+
+$access_token = 'tYO69OAFrf1t8TOcRLmVCjqdaMLx3fMXgm4YGlvANWE56EjBjolij67ND422KmII/IXz+YfqhRg9+0vfIHiMsgYsUeHy0H5O6mxOiKbXmXRc3QMfBN2a57IVy/kfJDpT2aWNRtTJ3qMrkQvHcAkb0wdB04t89/1O/w1cDnyilFU=';
+//$proxy = <YOUR_PROXY_FROM_FIXIE>;
+//$proxyauth = <YOUR_PROXYAUTH_FROM_FIXIE>;
+
+// Get POST body content
+$content = file_get_contents('php://input');
+
+// Parse JSON
+$events = json_decode($content, true);
+
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+        // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get text sent
+            $text = $event['message']['text'];
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+
+            // Build message to reply back
+            // Enter your code here 
+
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer '.$access_token);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            echo $result."\r\n";
+        }
+    }
+}
+echo 'OK';
