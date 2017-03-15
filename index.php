28132be5e714fa110c78af2809d58ef8f610f9f3
571998011 <thanapoom.son@crru.ac.th>
2017-03-15 17:07:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'tYO69OAFrf1t8TOcRLmVCjqdaMLx3fMXgm4YGlvANWE56EjBjolij67ND422KmII/IXz+YfqhRg9+0vfIHiMsgYsUeHy0H5O6mxOiKbXmXRc3QMfBN2a57IVy/kfJDpT2aWNRtTJ3qMrkQvHcAkb0wdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
-$content = file_get_contents('php://input');
+$content = file_get_contents('http://api.wunderground.com/api/95287c04f936abda/forecast5day/q/TH/nonthaburi.json');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
@@ -11,15 +11,6 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			//$event = {"text":"yass"}
-			 $ch1 = curl_init();
-            curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch1, CURLOPT_URL, 'https://th.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='.$text_ex[1]);
-            $result1 = curl_exec($ch1);
-            curl_close($ch1);
-            
-            $obj = json_decode($result1, true);
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
@@ -35,8 +26,7 @@ if (!is_null($events['events'])) {
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
-			
-			$post = json_encode($obj);
+			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
