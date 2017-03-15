b60a6c4af461221bb9a91ae023a7693d9888a57d
571998011 <thanapoom.son@crru.ac.th>
2017-03-15 15:44:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,13 +17,13 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
+			/*$messages = [
 				'type' => 'text',
 				'text' => $text
-				'te' => 'text',
-				'te' => $text
-			];
-
+			];*/
+			$messages = [
+				'type'=> 'http://api.wunderground.com/api/95287c04f936abda/forecast10day/q/TH/nonthaburi.json'
+				];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
