d1633554392fe268eef11d6adbcdc0253e52aacf
571998011 <thanapoom.son@crru.ac.th>
2017-03-15 14:58:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 				'text' => $text
-				'type' => 'text',
-				'text' => $text
+				'te' => 'text',
+				'te' => $text
 			];
 
 
