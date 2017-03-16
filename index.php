e95353ca434d086d281cdc62866951bdf5a9a28e
571998011 <thanapoom.son@crru.ac.th>
2017-03-16 11:04:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'http://api.wunderground.com/api/95287c04f936abda/forecast10day/q/TH/nonthaburi.json'
+				'text' => "http://api.wunderground.com/api/95287c04f936abda/forecast10day/q/TH/nonthaburi.json"
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
