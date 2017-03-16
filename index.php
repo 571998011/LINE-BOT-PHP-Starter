468cc4b612e609a7815ff5cb746ae60d7c9c5c50
571998011 <thanapoom.son@crru.ac.th>
2017-03-17 04:22:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -60,8 +60,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $value.$value1	
 			];
-			'type'=>'image',
-			'image'=> $value2
+
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
