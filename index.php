d2e3a534076c0cf6cbe3164cc9043ebeecdf2079
571998011 <thanapoom.son@crru.ac.th>
2017-03-16 16:42:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,16 @@
     } else {
         echo 'there has been an error connecting';
     } 
+$sql = "SELECT* FROM hardware_info";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo $row["number"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+pg_close($conn);
 ?>
