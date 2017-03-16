6cec6184aef83be9a48c60cafa58d5ae99ff0d58
571998011 <thanapoom.son@crru.ac.th>
2017-03-16 16:28:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,69 +1,18 @@
 <?php
-$access_token = 'tYO69OAFrf1t8TOcRLmVCjqdaMLx3fMXgm4YGlvANWE56EjBjolij67ND422KmII/IXz+YfqhRg9+0vfIHiMsgYsUeHy0H5O6mxOiKbXmXRc3QMfBN2a57IVy/kfJDpT2aWNRtTJ3qMrkQvHcAkb0wdB04t89/1O/w1cDnyilFU=';
-// Get POST body content
-$host = "ec2-54-243-214-198.compute-1.amazonaws.com";
-$port = "5432";
-$credentials = "fxujqiwvxjhugr 7eb01f27f07a9bb76a450401c6322a5671325458ba787719ace0d7df498caf36";
-$dbname = "d8hsko3c4c4lhj";
-$db = pg_connect( "$host $port $dbname $credentials"  );
-if (!$db) {
-    echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
-   $sql =<<<EOF
-      SELECT * from weather;
-EOF;
+// make our connection
+$connection = pg_connect("host=ec2-54-243-214-198.compute-1.amazonaws.com dbname=d8hsko3c4c4lhj user=fxujqiwvxjhugr password=7eb01f27f07a9bb76a450401c6322a5671325458ba787719ace0d7df498caf36");
 
-   $ret = pg_query($db, $sql);
-   if(!$ret){
-      echo pg_last_error($db);
-      exit;
-   } 
-   while($row = pg_fetch_row($ret)){
-      echo "temp = ". $row[0] . "\n";
-   }
-   echo "Operation done successfully\n";
-   pg_close($db);
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $result1
-			];
-			
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			
-			echo $result . "\r\n";
-		}
-	}
+// let me know if the connection fails
+if (!$connection) {
+    print("Connection Failed.");
+    exit;
 }
-echo "OK";
+
+// declare my query and execute
+$myresult = pg_exec($connection, "SELECT * FROM weather");
+
+// process results
+$myvalue = pg_result($myresult);
+
+// print results
+print("My Value: $myvalue<br />\n");
