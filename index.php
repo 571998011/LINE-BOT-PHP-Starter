8b670b7e07d20e1d9dc0e649dda4eaec1a2f95d7
571998011 <thanapoom.son@crru.ac.th>
2017-03-16 16:56:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $conn = pg_connect("host='ec2-54-235-181-120.compute-1.amazonaws.com' port='5432
 // Check connection
 $value = "";
 
-$sql = " select * from hardware_info";
+$sql = " SELECT * FROM hardware_info ORDER BY datetime DESC LIMIT 1;";
 $result = pg_query($conn, $sql);
 
 if (pg_num_rows($result) >= 0) {
