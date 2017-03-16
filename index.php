b0d8c5b04fb848bffd8395513fca479fa2e63a86
571998011 <thanapoom.son@crru.ac.th>
2017-03-16 11:46:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,7 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => 'CURLOPT_URL'
-			];
+			
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -29,15 +26,19 @@ if (!is_null($events['events'])) {
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_URL, 'http://api.wunderground.com/api/95287c04f936abda/forecast10day/q/TH/nonthaburi.json'.'text');
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
+			$messages = [
+				'type' => 'text',
+				'text' => $result
+			];
 			echo $result . "\r\n";
 		}
 	}
