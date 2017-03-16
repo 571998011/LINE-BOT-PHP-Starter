4d6395884f5758d17401055aebce33d90a4a7999
571998011 <thanapoom.son@crru.ac.th>
2017-03-16 11:51:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $result
+				'text' => 'kkk'
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
