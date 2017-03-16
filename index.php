487229258b8bb84269faea03747b5d261cfa5aee
571998011 <thanapoom.son@crru.ac.th>
2017-03-16 16:45:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
-    $connection = pg_connect ("host=ec2-54-235-181-120.compute-1.amazonaws.com dbname=dd9sbv2pl3npfu user=zeczwoatxgggff password=2c64a703a8847eeebc479d4a21119d2868fb77d2c637b39e209c4c8088883fee");
-    if($connection) {
-       echo 'connected';
-    } else {
-        echo 'there has been an error connecting';
-    } 
+$servername = "ec2-54-235-181-120.compute-1.amazonaws.com";
+$username = "zeczwoatxgggff";
+$password = "2c64a703a8847eeebc479d4a21119d2868fb77d2c637b39e209c4c8088883fee";
+$dbname = "dd9sbv2pl3npfu";
+error_reporting(0);
+$conn = pg_connect("host='ec2-54-235-181-120.compute-1.amazonaws.com' port='5432' dbname='dd9sbv2pl3npfu' user='zeczwoatxgggff' password='2c64a703a8847eeebc479d4a21119d2868fb77d2c637b39e209c4c8088883fee'");
+// Check connection
+
+
 $sql = " select * from hardware_info";
 $result = pg_query($conn, $sql);
 
@@ -16,5 +19,6 @@ if (pg_num_rows($result) >= 0) {
 } else {
     echo "0 results";
 }
+
 pg_close($conn);
 ?>
