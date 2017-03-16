3e34d007ca269c26e0c439769147ce6c448dcf88
571998011 <thanapoom.son@crru.ac.th>
2017-03-17 03:54:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -71,6 +71,7 @@ if (!is_null($events['events'])) {
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
+				'messages' => [$messages1],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
