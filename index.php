81f37c01aa409f444d243e7a36e3b611f0aa8ed1
571998011 <thanapoom.son@crru.ac.th>
2017-03-17 07:16:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -71,8 +71,8 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages.$messages1],
-			
+				'messages' => [$messages],
+				'messages' => [$messages1]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
